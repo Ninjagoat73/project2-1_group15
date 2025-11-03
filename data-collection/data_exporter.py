ecbb3f461e5f4d5fd894c2178e29d2744180aa5c
zeteny levay <levay.zeteny3@gmail.com>
2025-11-03 21:10:59 +0100

    yaml files read

--- data-collection/data_exporter.py
+++ data-collection/data_exporter.py
@@ -1,13 +1,18 @@
 import csv
 import datetime
 from hardware_info_getter import get_all_data
+from yaml_reader import extract_static_yaml_value
 
 def create_run_id(training_type):
     now_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     run_id = f"{training_type}-{now_str}"
     return run_id
 
-def create_static_csv(run_id: str, hardware_data: dict, ):
+def create_static_csv(run_id: str, hardware_data: dict, yaml_data: dict ):
+
+
+    print(hardware_data)
+    print(yaml_data)
 
     header = [
         'run_id', 'os', 'cpu', 'physical_cores', 'system_ram_gb',
@@ -15,9 +20,9 @@ def create_static_csv(run_id: str, hardware_data: dict, ):
         'learning_rate', 'buffer_size', 'beta', 'episolon', 'lambd',
         'num_epochs', 'learning_rate_schedule', 'normalize', 'hidden_units',
         'num_layers', 'vis_encode_type', 'reward_extrinsic_gamma',
-        'reward_extrinsic_strength', 'keep_checkpoints', 'max_step',
+        'reward_extrinsic_strength', 'keep_checkpoints', 'max_steps',
         'time_horizon', 'summary_freq', 'sequence_length', 'memory_size',
-        'curiosity_gamma', 'curiosity_strenght', 'curiosity_hidden_units',
+        'curiosity_gamma', 'curiosity_strength', 'curiosity_hidden_units',
         'curiosity_learning_rate', 'save_steps', 'team_change', 'swap_steps',
         'window', 'play_againts_latest_model_ratio', 'initial_elo'
     ]
@@ -33,9 +38,37 @@ def create_static_csv(run_id: str, hardware_data: dict, ):
         'system_ram_gb': hardware_data.get('system_ram_gb'),
         'gpu': hardware_data.get('gpu'),
         'GPU_ram_mb': hardware_data.get('GPU_ram_mb'),
-        'training_type': "ppo",
-        'batch_size': 1,
-        'learning_rate': 2
+        'training_type': yaml_data.get('training_type'),
+        'batch_size': yaml_data.get('batch_size'),
+        'learning_rate': yaml_data.get('learning_rate'),
+        'buffer_size': yaml_data.get('buffer_size'),
+        'beta': yaml_data.get('beta'),
+        'episolon': yaml_data.get('episolon'),
+        'lambd': yaml_data.get('lambd'),
+        'num_epochs': yaml_data.get('num_epochs'),
+        'learning_rate_schedule': yaml_data.get('learning_rate_schedule'),
+        'normalize': yaml_data.get('normalize'),
+        'hidden_units': yaml_data.get('hidden_units'),
+        'num_layers': yaml_data.get('num_layers'),
+        'vis_encode_type': yaml_data.get('vis_encode_type'),
+        'reward_extrinsic_gamma': yaml_data.get('reward_extrinsic_gamma'),
+        'reward_extrinsic_strength': yaml_data.get('reward_extrinsic_strength'),
+        'keep_checkpoints': yaml_data.get('keep_checkpoints'),
+        'max_steps': yaml_data.get('max_steps'),
+        'time_horizon': yaml_data.get('time_horizon'),
+        'summary_freq': yaml_data.get('summary_freq'),
+        'sequence_length': yaml_data.get('sequence_length'),
+        'memory_size': yaml_data.get('memory_size'),
+        'curiosity_gamma': yaml_data.get('curiosity_gamma'),
+        'curiosity_strength': yaml_data.get('curiosity_strength'),
+        'curiosity_hidden_units': yaml_data.get('curiosity_hidden_units'),
+        'curiosity_learning_rate': yaml_data.get('curiosity_learning_rate'),
+        'save_steps': yaml_data.get('save_steps'),
+        'team_change': yaml_data.get('team_change'),
+        'swap_steps': yaml_data.get('swap_steps'),
+        'window': yaml_data.get('window'),
+        'play_againts_latest_model_ratio': yaml_data.get('play_againts_latest_model_ratio'),
+        'initial_elo': yaml_data.get('initial_elo')
     }
 
     try:
@@ -83,6 +116,10 @@ def create_summary_csv(run_id: str, hardware_data: dict ):
 
 def create_all_csv():
     hardware_data = get_all_data()
-    training_type = "ppo"
+    yaml_data = extract_static_yaml_value("E:/project2-1/config/ppo/3DBall.yaml")
+    training_type = yaml_data['training_type']
     run_id = create_run_id(training_type)
-    create_static_csv(run_id, hardware_data)
+    create_static_csv(run_id, hardware_data, yaml_data)
+
+
+create_all_csv()
--- data-collection/yaml_reader.py
+++ data-collection/yaml_reader.py
@@ -6,29 +6,50 @@ header = [
     'learning_rate', 'buffer_size', 'beta', 'episolon', 'lambd',
     'num_epochs', 'learning_rate_schedule', 'normalize', 'hidden_units',
     'num_layers', 'vis_encode_type', 'reward_extrinsic_gamma',
-    'reward_extrinsic_strength', 'keep_checkpoints', 'max_step',
+    'reward_extrinsic_strength', 'keep_checkpoints', 'max_steps',
     'time_horizon', 'summary_freq', 'sequence_length', 'memory_size',
-    'curiosity_gamma', 'curiosity_strenght', 'curiosity_hidden_units',
+    'curiosity_gamma', 'curiosity_strength', 'curiosity_hidden_units',
     'curiosity_learning_rate', 'save_steps', 'team_change', 'swap_steps',
     'window', 'play_againts_latest_model_ratio', 'initial_elo'
 ]
 
-def extract_static_yaml_value(yaml_data: dict):
+def extract_static_yaml_value(filename):
+
+    with open(filename, 'r') as f:
+        full_yaml = yaml.safe_load(f)
 
     try:
-        layer1 = next(iter(yaml_data.values()))
+        layer1 = next(iter(full_yaml.values()))
         data = next(iter(layer1.values()))
 
 
         dict_data = {}
 
 
-        training_type = data['trainer_type']
+        dict_data.update({'training_type': data['trainer_type']})
+        dict_data.update({'keep_checkpoints': data['keep_checkpoints']})
+        dict_data.update({'max_steps': data['max_steps']})
+        dict_data.update({'time_horizon': data['time_horizon']})
+        dict_data.update({'summary_freq': data['summary_freq']})
         for key, value in data['hyperparameters'].items():
             dict_data.update({key: value})
-            print(key, value)
         for key, value in data['network_settings'].items():
             dict_data.update({key: value})
+        for key, value in data['reward_signals']['extrinsic'].items():
+            dict_data.update({key: value})
+
+        try:
+            for key, value in data['reward_signals']['curiosity'].items():
+                curiosity_key = 'curiosity_' + key
+                dict_data.update({curiosity_key: value})
+            for key, value in data['self_play'].items():
+                dict_data.update({key: value})
+        except:
+            pass
+
+
+        return dict_data
+
 
 
     except (StopIteration,AttributeError) as e:
@@ -36,10 +57,6 @@ def extract_static_yaml_value(yaml_data: dict):
         print(e)
 
 
+if __name__ == '__main__':
+    extract_static_yaml_value("E:/project2-1/config/ppo/3DBall.yaml")
 
-with open("E:/project2-1/config/ppo/3DBall.yaml") as file:
-    try:
-        extracted_data = yaml.safe_load(file)
-        cleaned_data = extract_static_yaml_value(extracted_data)
-    except yaml.YAMLError as exc:
-        print(exc)
