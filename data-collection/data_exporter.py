561167c2e60f71b8121d95cdad98c138332b08ae
zeteny levay <levay.zeteny3@gmail.com>
2025-11-03 15:57:35 +0100

    etc

--- data-collection/data_exporter.py
+++ data-collection/data_exporter.py
@@ -7,7 +7,7 @@ def create_run_id(training_type):
     run_id = f"{training_type}-{now_str}"
     return run_id
 
-def create_static_csv(hardware_data: dict, run_id: str):
+def create_static_csv(run_id: str, hardware_data: dict, ):
 
     header = [
         'run_id', 'os', 'cpu', 'physical_cores', 'system_ram_gb',
@@ -47,15 +47,34 @@ def create_static_csv(hardware_data: dict, run_id: str):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-def create_training_csv():
-    pass
+def create_training_csv(run_id: str, hardware_data: dict):
 
-def create_summary_csv():
-    pass
+
+    header = [
+        'run_id', 'cpu_frequency_mhz', 'cpu_usage_percent', 'gpu_usage_percent', 'vram_usage_mb',
+        'ram_usage_mb', 'episode', 'step', 'agent_id', 'reward', 'stability_reward', 'loss_value', 'entropy', 'policy_loss', 'value_estimate'
+        'cumulative_reward', 'reward_mean_100', 'time_elapsed_ms', 'steps_per_second'
+    ]
+
+    filename = f"{run_id}-training.csv"
+
+    data_dict = {
+
+    }
+
+def create_summary_csv(run_id: str, hardware_data: dict ):
+
+    header = [
+        'run_id', 'final_reward_mean', 'max_reward_mean', 'time_to_max_reward_sec', 'total_duration_sec',
+        'mean_policy_loss', 'mean_value_loss', 'mean_entropy', 'batch_size', 'final_entropy', 'max_cpu_usage_percent', 'mean_cpu_usage_percent',
+        'max_ram_used_mb', 'mean_ram_used_mb', 'max_gpu_usage_percent', 'mean_gpu_usage_percent', 'max_vram_used_mb', 'mean_vram_used_mb'
+    ]
+
+    filename = f"{run_id}-summary.csv"
 
 
 def create_all_csv():
     hardware_data = get_all_data()
     training_type = "ppo"
     run_id = create_run_id(training_type)
-    create_static_csv(hardware_data, run_id)
+    create_static_csv(run_id, hardware_data)
