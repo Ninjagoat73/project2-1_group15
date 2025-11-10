37075560ab8668bdfc2cd79305b018d7e9b38e6f
PedroPianna <98548259+PedroPianna@users.noreply.github.com>
2025-11-11 00:19:23 +0100

    first version of training script
    this is a very basic version: it only supports 3DBall and one type of unity executable (not possible to train 3DBall and FoodCollector, for example)

---
+++ data-collection/Readme.md
@@ -0,0 +1,7 @@
+## Instructions for automated training
+
+### The current script is very limited and only supports 3DBall training. I will work on making different types of agents compatible. To setup the environment, you will need to:
+1. [Create the 3DBall executable](https://docs.unity3d.com/Packages/com.unity.ml-agents@4.0/manual/Learning-Environment-Executable.html)
+2. Create a folder with multiple 3DBall yaml files
+
+### To run the script, simply use python3 training_script.py <YAML-folder> <3DBall-executable>, where YAML-folder is a folder containing multiple YAML files (for 3DBall currently) and 3DBall-executable is the executabl generated in unity. YAML file generation is not yet complete, we are still waiting for [issue #69](https://github.com/Ninjagoat73/project2-1_group15/issues/69) to be closed so we can actually start training
---
+++ data-collection/training_script.py
@@ -0,0 +1,45 @@
+import data_exporter
+import os
+import sys
+import datetime
+import subprocess
+
+"""
+    for each file in folder:
+        read yaml
+        create run_id
+        call data_exporter to add static.csv entry
+        run environment executable (terminal?) to execute training
+"""
+def create_run_id(yaml_path: str):
+    now_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    run_id = f"{yaml_path}-{now_str}"
+    return run_id
+
+def run_training(yaml_path: str, run_id: str, executable_path: str):
+    data_exporter.create_static_csv(run_id, yaml_path)
+    subprocess.run([
+        "mlagents-learn",
+        "/Users/pedropianna/project2-1_group15/data-collection/test/3DBall.yaml",
+        "--env=" + executable_path, # TODO: extract the type from yaml file
+        "--run-id=20251110-235215",
+        "--no-graphics"
+    ])
+
+if len(sys.argv) < 3:
+    print("Usage: python3 training_scipt.py <path-to-yaml-folder> <path-to-executable>")
+    sys.exit(0)
+
+folder_path = sys.argv[1]
+executable_path = sys.argv[2]
+
+if not os.path.isdir(folder_path):
+    print(f"Error: '{folder_path}' is not a valid directory.")
+    sys.exit(0)
+
+for yaml in os.listdir(folder_path):
+    run_id = create_run_id(yaml)
+    yaml_path = os.path.join(folder_path, yaml)
+    if os.path.isfile(yaml_path):
+        print(yaml_path)
+        run_training(yaml_path, run_id, executable_path)
