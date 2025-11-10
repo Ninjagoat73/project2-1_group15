d0f269391edb282a429e839a1ae2153f08cc4271
PedroPianna <98548259+PedroPianna@users.noreply.github.com>
2025-11-10 13:49:56 +0100

    added documentation and function now returns the status (success or not)

--- data-collection/data_exporter.py
+++ data-collection/data_exporter.py
@@ -5,6 +5,11 @@ from yaml_reader import extract_static_yaml_value
 from static_dict import create_static_dict, header
 
 def create_static_csv(run_id: str, yaml_path: str):
+    """
+        This function takes a run_id and the path of a YAML file as parameters and adds that information, as well as hardware data, as an entry to the static.csv file
+        Returns: boolean stating if operation was successful
+    """
+    success = True
     yaml_data = extract_static_yaml_value(yaml_path)
     hardware_data = get_all_data()
     print("Hardware data to be added to entry: ", hardware_data)
@@ -24,8 +29,11 @@ def create_static_csv(run_id: str, yaml_path: str):
                 csv_writer.writerow(data_dict)
     except IOError as e:
         print(f"Error: Could not write to file {filename}. {e}")
+        success = False
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+        success = False
+    return success
 
 if __name__ == "__main__":
     create_static_csv("run_id_1", "../config/ppo/FoodCollector.yaml")
