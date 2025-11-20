1d46f756f24bb331c53ed543dcaac530619d30a7
PedroPianna <98548259+PedroPianna@users.noreply.github.com>
2025-11-20 12:30:55 +0100

    changed training script so that YAML files are generated every single time and old ones are deleted

--- data-collection/training_script.py
+++ data-collection/training_script.py
@@ -5,6 +5,7 @@ import sys
 import datetime
 import subprocess
 import yaml_generator
+import shutil
 
 def run_training(yaml_path: str, run_id: str, executable_path: str):
     result = subprocess.run([
@@ -12,7 +13,7 @@ def run_training(yaml_path: str, run_id: str, executable_path: str):
         yaml_path,
         "--env=" + executable_path,
         "--run-id=" + run_id,
-#        "--no-graphics"
+        "--no-graphics"
     ])
     if result.returncode != 0:
         print("Training failed. No entry will be added to static.csv")
@@ -23,7 +24,12 @@ if len(sys.argv) < 2:
     print("Usage: python3 training_scipt.py <path-to-executable-folder>")
     sys.exit(0)
 
+if os.path.isdir("generated"):
+    print(f"YAML folder already exists... Deleting folder and generating new YAMLs...")
+    shutil.rmtree("generated")
+
 yamls = yaml_generator.generate_batch()
+
 executable_folder_path = sys.argv[1]
 
 if not os.path.isdir(executable_folder_path):
