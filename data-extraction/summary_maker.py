c3fcb1c90c4d53890d3cb4be61391f40d4cd6d97
zeteny levay <levay.zeteny3@gmail.com>
2025-11-23 22:29:38 +0100

    summary_maker.py, works and gets correct data

--- data-extraction/summary_maker.py
+++ data-extraction/summary_maker.py
@@ -4,11 +4,12 @@ from pathlib import Path
 import pandas as pd
 
 header = [
-    'run_id', 'final_reward_mean', 'max_reward_mean', 'time_to_max_reward_sec', 'total_duration_sec',
+    'run_id', 'time_to_max_reward_sec', 'total_duration_sec',
     'mean_policy_loss', 'mean_value_loss', 'mean_entropy', 'batch_size', 'final_entropy', 'max_cpu_usage_percent',
     'mean_cpu_usage_percent',
     'max_ram_used_mb', 'mean_ram_used_mb', 'max_gpu_usage_percent', 'mean_gpu_usage_percent', 'max_vram_used_mb',
-    'mean_vram_used_mb'
+    'mean_vram_used_mb', 'max_reward', 'mean_cpu_frequency', 'max_cpu_frequency', 'final_reward',
+    'max_game_cpu_usage_percent', 'max_game_memory_usage_mb', 'mean_game_cpu_usage_percent', 'mean_game_memory_usage_mb'
 ]
 
 
@@ -32,34 +33,43 @@ def summarize(target_run_id):
         "run_id": target_run_id,
         "final_reward": df["cumulative_reward"].iloc[-1],
         "max_reward": df["cumulative_reward"].max() ,
-        "time_to_max_reward_sec": None,
-        "total_duration_sec":  None,
+        "time_to_max_reward_sec": df["time_elapsed(s)"].iloc[df["cumulative_reward"].idxmax()],
+        "total_duration_sec":  df["time_elapsed(s)"].max(),
 
         "mean_policy_loss": df["policy_loss"].mean() ,
         "mean_value_loss": df["loss_value"].mean(),
         "mean_entropy": df["entropy"].mean() ,
         "final_entropy": df["entropy"].iloc[-1] ,
 
-        "max_cpu_usage_percent": df["cpu_usage"].max(),
-        "mean_cpu_usage_percent": df["cpu_usage"].mean(),
+        "max_cpu_usage_percent": df["cpu_usage_percent"].max(),
+        "mean_cpu_usage_percent": df["cpu_usage_percent"].mean(),
 
-        "max_ram_used_mb": df["ram_used_mb"].max(),
-        "mean_ram_used_mb": df["ram_used_mb"].mean(),
+        "max_cpu_frequency": df["cpu_frequency_mhz"].max(),
+        "mean_cpu_frequency": df["cpu_frequency_mhz"].mean(),
 
-        "max_gpu_usage_percent": df["gpu_usage"].max() if "gpu_usage" in df else None,
-        "mean_gpu_usage_percent": df["gpu_usage"].mean() if "gpu_usage" in df else None,
+        "max_ram_used_mb": df["ram_usage_mb"].max(),
+        "mean_ram_used_mb": df["ram_usage_mb"].mean(),
 
-        "max_vram_used_mb": df["vram_used_mb"].max() if "vram_used_mb" in df else None,
-        "mean_vram_used_mb": df["vram_used_mb"].mean() if "vram_used_mb" in df else None,
+        "max_gpu_usage_percent": df["gpu_usage_percent"].max(),
+        "mean_gpu_usage_percent": df["gpu_usage_percent"].mean() ,
+
+        "max_vram_used_mb": df["vram_usage_mb"].max(),
+        "mean_vram_used_mb": df["vram_usage_mb"].mean(),
+
+        "mean_game_cpu_usage_percent": df["game_cpu_usage"].mean(),
+        "max_game_cpu_usage_percent": df["game_cpu_usage"].max(),
+
+        "mean_game_memory_usage_mb": df["game_memory_usage(MB)"].mean(),
+        "max_game_memory_usage_mb": df["game_memory_usage(MB)"].max(),
     }
 
     try:
         if summary_path.is_file():
-            with open(summary_path, 'a', newline='') as csvfile:
+            with open(summary_path, 'a', newline='', encoding='utf-8') as csvfile:
                 csv_writer = csv.DictWriter(csvfile, fieldnames=header)
                 csv_writer.writerow(result)
         else:
-            with open(summary_path, 'w', newline='') as csvfile:
+            with open(summary_path, 'w', newline='', encoding='utf-8') as csvfile:
                 csv_writer = csv.DictWriter(csvfile, fieldnames=header)
                 csv_writer.writeheader()
                 csv_writer.writerow(result)
@@ -72,4 +82,4 @@ def summarize(target_run_id):
 
     return success
 
-summarize("first_run/3DBall")
+summarize("20251123-014754/3DBall")
