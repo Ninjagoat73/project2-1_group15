d8fa2b3c329c8725b3b4301f60f5b1bd3380ad6a
PedroPianna <98548259+PedroPianna@users.noreply.github.com>
2026-01-18 18:39:24 +0100

    rq2

--- data-extraction/cleaning.py
+++ data-extraction/cleaning.py
@@ -1,8 +1,8 @@
-﻿import pandas as pd
+import pandas as pd
 
 # 1. Load the Master List (static.csv)
 print("Loading master static data...")
-static_df = pd.read_csv('E:/project2-1/data-collection/data/static.csv')
+static_df = pd.read_csv('../data-collection/data/static.csv')
 
 # Get a unique list of valid RunIDs (this is our 'Golden List')
 valid_run_ids = set(static_df['RunID'].unique())
@@ -27,7 +27,7 @@ def clean_large_file(file_path, valid_ids):
 
 
 # 2. Clean Training and Summary files
-clean_large_file('E:/project2-1/data-collection/data/training.csv', valid_run_ids)
-clean_large_file('E:/project2-1/data-collection/data/summary.csv', valid_run_ids)
+clean_large_file('../data-collection/data/training.csv', valid_run_ids)
+clean_large_file('../data-collection/data/summary.csv', valid_run_ids)
 
 print("\nAll files are now synchronized with static.csv!")
