5dd98e28717e141f4daef92951767ede059fe3cc
zeteny levay <levay.zeteny3@gmail.com>
2026-01-21 12:27:20 +0100

    models

--- data-analysis/rq3/model2.py
+++ data-analysis/rq3/model2.py
@@ -1,4 +1,4 @@
-import random
+﻿import random
 
 import pandas as pd
 import numpy as np
@@ -29,87 +29,57 @@ r2_train_dict = {}
 feature_cols = [
     'batch_size', 'learning_rate', 'buffer_size', 'beta',
     'epsilon', 'lambd', 'num_epoch', 'learning_rate_schedule',
-    'normalize', 'hidden_units', 'num_layers', 'max_steps', 'game_name',
+    'normalize', 'hidden_units', 'num_layers', 'max_steps',
 ]
 
 optimal_settings = []
 
 merged_df['scaled_reward'] = merged_df.groupby('game_name')['final_reward'].transform(scale_group)
-
-X_global = merged_df[feature_cols].copy()
-y_global = merged_df['scaled_reward'].fillna(0)
-
-
-
-le_game = LabelEncoder()
-X_global['game_name'] = le_game.fit_transform(X_global['game_name'])
-
-le_sched = LabelEncoder()
-X_global['learning_rate_schedule'] = le_sched.fit_transform(X_global['learning_rate_schedule'])
-
-X_train, X_test, y_train, y_test = train_test_split(X_global, y_global, test_size=0.2, random_state= 1 , stratify=merged_df['game_name'])
-regr = RandomForestRegressor(n_estimators=100, max_depth=10, min_samples_leaf=5, random_state=1)
-regr.fit(X_train, y_train)
-y_train_pred = regr.predict(X_train)
-global_train_r2 = r2_score(y_train, y_train_pred)
-print(f"Global Training R2: {global_train_r2:.2f}")
 game_counts = static_df["game_name"].value_counts()
 
 
 
 for game in games:
 
-    # game_df = merged_df[merged_df['game_name'] == game].copy()
-    game_id = le_game.transform([game])[0]
-
-    game_mask = X_test['game_name'] == game_id
-
-    if not game_mask.any():
-        print(f"-----------{game}------------")
-        print("Not enough samples in test set to calculate R2.")
-        r2_dict[game] = 0
-        r2_train_dict[game] = 0
-        continue
+    game_df = merged_df[merged_df['game_name'] == game].copy()
 
 
 
-    # X = game_df[feature_cols].copy()
-    # y = game_df['final_reward']
-    # y = y.fillna(0)
+    X = game_df[feature_cols].copy()
+    y = game_df['scaled_reward']
+    y = y.fillna(0)
 
-    # le = LabelEncoder()
-    # X['learning_rate_schedule'] = le.fit_transform(X['learning_rate_schedule'])
-    # X['normalize'] = X['normalize'].astype(int)
+    le = LabelEncoder()
+    X['learning_rate_schedule'] = le.fit_transform(X['learning_rate_schedule'])
+    X['normalize'] = X['normalize'].astype(int)
 
 
 
-    # X = X.fillna(0)
+    X = X.fillna(0)
 
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
 
-    # regr = RandomForestRegressor(n_estimators=100, max_depth=7,
-    # min_samples_leaf=5,
-    # max_features='sqrt',
-    # random_state=1)
-    # regr.fit(X_train, y_train)
+    regr = RandomForestRegressor(n_estimators=100, max_depth=10,
+    min_samples_leaf=5,
+    random_state=1)
+    regr.fit(X_train, y_train)
 
-    game_X_test = X_test[game_mask]
-    game_y_test = y_test[game_mask]
 
-    predictions = regr.predict(game_X_test)
 
-    # r2_train = r2_score(y_train, regr.predict(X_train))
-    r2 = r2_score(game_y_test, predictions)
-    mae = mean_absolute_error(game_y_test, predictions)
+    predictions = regr.predict(X_test)
+
+    r2_train = r2_score(y_train, regr.predict(X_train))
+    r2 = r2_score(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
 
 
 
     r2_dict[game] = r2
-    # r2_train_dict[game] = r2_train
+    r2_train_dict[game] = r2_train
 
     print(f"-----------{game}------------")
     print(f"Prediction Accuracy (R2): {r2:.2f}")
-    # print(f"Prediction Accuracy Training (R2 Training): {r2_train:.2f}")
+    print(f"Prediction Accuracy Training (R2 Training): {r2_train:.2f}")
     print(f"Average Error (MAE): {mae:.2f}")
     print(f"Game Counts: {game_counts[game]:.2f}")
 
@@ -126,15 +96,15 @@ for game in games:
 
         for col in feature_cols:
             if col == 'learning_rate_schedule':
-                search_space[col] = np.random.choice(X_global[col].unique(), samples)
+                search_space[col] = np.random.choice(X[col].unique(), samples)
             elif col == 'max_steps':
                 search_space[col] = np.random.choice([100000, 250000, 500000, 1000000, 2000000], samples)
             elif col == 'normalize':
                 search_space[col] = np.random.choice([0, 1], samples)
             elif col in ['num_layers', 'hidden_units', 'num_epoch']:
-                search_space[col] = np.random.randint(X_global[col].min(), X_global[col].max() + 1, samples)
+                search_space[col] = np.random.randint(X[col].min(), X[col].max() + 1, samples)
             else:
-                search_space[col] = np.random.uniform(X_global[col].min(), X_global[col].max(), samples)
+                search_space[col] = np.random.uniform(X[col].min(), X[col].max(), samples)
 
         made_X =pd.DataFrame(search_space)
         made_X = made_X[feature_cols]
@@ -145,7 +115,7 @@ for game in games:
 
 
 
-        best_hyperparameters['learning_rate_schedule'] = le_sched.inverse_transform([int(best_hyperparameters['learning_rate_schedule'])])[0]
+        best_hyperparameters['learning_rate_schedule'] = le.inverse_transform([int(best_hyperparameters['learning_rate_schedule'])])[0]
 
         best_hyperparameters['game_name'] = game
         best_hyperparameters['model_r2'] = r2
@@ -162,15 +132,15 @@ for game in games:
 
         for col in feature_cols:
             if col == 'learning_rate_schedule':
-                search_space[col] = np.random.choice(X_global[col].unique(), samples)
+                search_space[col] = np.random.choice(X[col].unique(), samples)
             elif col == 'max_steps':
                 search_space[col] = np.random.choice([100000, 250000, 500000, 1000000, 2000000], samples)
             elif col == 'normalize':
                 search_space[col] = np.random.choice([0, 1], samples)
             elif col in ['num_layers', 'hidden_units', 'num_epoch']:
-                search_space[col] = np.random.randint(X_global[col].min(), X_global[col].max() + 1, samples)
+                search_space[col] = np.random.randint(X[col].min(), X[col].max() + 1, samples)
             else:
-                search_space[col] = np.random.uniform(X_global[col].min(), X_global[col].max(), samples)
+                search_space[col] = np.random.uniform(X[col].min(), X[col].max(), samples)
 
         made_X =pd.DataFrame(search_space)
         made_X = made_X[feature_cols]
@@ -181,7 +151,7 @@ for game in games:
         mae_text = f"{mae:.2f}(MAE)"
 
 
-        best_hyperparameters['learning_rate_schedule'] = le_sched.inverse_transform([int(best_hyperparameters['learning_rate_schedule'])])[0]
+        best_hyperparameters['learning_rate_schedule'] = le.inverse_transform([int(best_hyperparameters['learning_rate_schedule'])])[0]
 
         best_hyperparameters['game_name'] = game
         best_hyperparameters['model_r2'] = mae_text
